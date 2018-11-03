8da65f3f367bb6cb2adf71fd89db82950d4770c5
huangsy <huangsy@feiyu.com>
2018-11-03 23:15:13 +0800

    完成滑动效果

--- DYNSegment/DYNSegmentView.m
+++ DYNSegment/DYNSegmentView.m
@@ -10,7 +10,7 @@
 #import "DYNMacro.h"
 #import "DYNSingleLabel.h"
 
-@interface DYNSegmentView()
+@interface DYNSegmentView() <UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate>
 
 @property (nonatomic, strong) UIScrollView *scrollerView;
 @property (nonatomic, strong) UICollectionView *collectionView;
@@ -43,6 +43,17 @@
     _flagView = [[UIView alloc] init];
     _flagView.backgroundColor = DYNColorBlue;
     [_scrollerView addSubview:_flagView];
+    
+    _collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:[self collectionViewlayout:0 height:0]];
+    _collectionView.pagingEnabled = YES;
+    _collectionView.showsVerticalScrollIndicator = NO;
+    _collectionView.showsHorizontalScrollIndicator = NO;
+    _collectionView.delegate = self;
+    _collectionView.dataSource = self;
+    _collectionView.backgroundColor = DYNColorWhite;
+    _collectionView.bounces = NO;
+    [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"UICollectionViewCell"];
+    [self addSubview:_collectionView];
 }
 
 - (void)layoutSubviews {
@@ -72,6 +83,13 @@
     _flagView.frameWidth = itemWidth;
     _flagView.frameHeight = 5;
     _flagView.frameEndY = DYNRulerViewHeight(_scrollerView, 1);
+    
+    _collectionView.frameOriginX = 0;
+    _collectionView.frameOriginY = _segmentHeight;
+    _collectionView.frameWidth = self.frameWidth;
+    _collectionView.frameHeight = self.frameHeight - _segmentHeight;
+    
+    [_collectionView setCollectionViewLayout:[self collectionViewlayout:_collectionView.frameWidth height:_collectionView.frameHeight]];
 }
 
 #pragma mark - private
@@ -99,12 +117,12 @@
     return _itemWidth;
 }
 
-- (void)updateflag {
+- (void)updateflag:(NSInteger)index {
     DYNWeakSelf(weakSelf)
     
     // 移动标志
     
-    CGFloat flagOriginX = self.itemWidth * self.currentItemIndex;
+    CGFloat flagOriginX = self.itemWidth * index;
     
     [UIView animateWithDuration:0.2 animations:^{
         weakSelf.flagView.frameOriginX = flagOriginX;
@@ -144,7 +162,7 @@
     // 缩放 item view
     
     for (UIView *itemView in _itemViews) {
-        if (_itemViews.count > _currentItemIndex && itemView == _itemViews[_currentItemIndex]) {
+        if (_itemViews.count > index && itemView == _itemViews[index]) {
             for (UIView *subView in itemView.subviews) {
                 if ([subView isKindOfClass:[DYNSingleLabel class]]) {
                     subView.layer.transform = CATransform3DMakeScale(1.3, 1.3, 1);
@@ -158,6 +176,22 @@
                 subView.layer.transform = CATransform3DMakeScale(1.0, 1.0, 1);
             }
         }
+    }
+}
+
+- (UICollectionViewLayout*)collectionViewlayout:(CGFloat)width height:(CGFloat)height {
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    flowLayout.minimumLineSpacing = 0;
+    flowLayout.minimumInteritemSpacing = 0;
+    flowLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    flowLayout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0);
+    flowLayout.itemSize = CGSizeMake(width, height);
+    return flowLayout;
+}
+
+- (void)updatePage:(NSInteger)index {
+    if (index < _items.count) {
+        [_collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForRow:index inSection:0] atScrollPosition:UICollectionViewScrollPositionNone animated:NO];
     }
 }
 
@@ -204,14 +238,54 @@
     
     [self layoutIfNeeded];
     
-    [self updateflag];
+    [self updateflag:_currentItemIndex];
+    [self updatePage:_currentItemIndex];
 }
 
 #pragma mark - action
 - (void)itemTap:(UITapGestureRecognizer*)tap {
     UIView *itemView = tap.view;
-    self.currentItemIndex = itemView.tag;
-    NSLog(@"%ld", itemView.tag);
+    
+    _currentItemIndex = itemView.tag;
+    
+    [self updateflag:itemView.tag];
+    [self updatePage:itemView.tag];
+}
+
+#pragma mark - delegate
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return _items.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"UICollectionViewCell" forIndexPath:indexPath];
+    
+    UIViewController *vc = [_delegate dynSegmentPageAtIndex:indexPath.row];
+    vc.view.frameOriginX = 0;
+    vc.view.frameOriginY = 0;
+    vc.view.frameWidth = cell.contentView.frameWidth;
+    vc.view.frameHeight = cell.contentView.frameHeight;
+    [cell.contentView addSubview:vc.view];
+    
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGPoint point = CGPointMake(scrollView.contentOffset.x + DYNRulerViewWidth(scrollView, 0.5), 0);
+    NSIndexPath *indexPath = [_collectionView indexPathForItemAtPoint:point];
+    
+    _currentItemIndex = indexPath.row;
+    
+    [self updateflag:indexPath.row];
 }
 
 @end
